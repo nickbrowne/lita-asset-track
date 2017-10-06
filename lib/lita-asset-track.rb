a5358e303f7af36ee35248734aac0e97afad4c90
nickbrowne <nickbrowne@users.noreply.github.com>
2017-10-06 15:59:38 +1100

    Add all the features

--- lib/lita-asset-track.rb
+++ lib/lita-asset-track.rb
@@ -1,9 +1,95 @@
 require 'lita'
+require 'lita-timing'
+require 'uri'
+require 'json'
+require 'net/http'
 
 module Lita
   module Handlers
     class AssetTrack < Handler
+      MINUTE = 60
+      HOUR = 60 * 60
+      MANIFEST_SET_KEY = "lita-asset-track-manifest"
+      EXTENSIONS = ["css", "js"]
 
+      route(/^asset\strack\slist/i, :list, command: true, help: { "asset track list" => "Lists tracked manifests" })
+      route(/^asset\strack\sadd\s+(.+)/i, :add, command: true, help: { "asset track add [url]" => "Provide one or more urls to a manifest.json to start tracking asset sizes in librato"})
+      route(/^asset\strack\sremove\s+(.+)/i, :remove, command: true, help: { "asset track remove [url]" => "Stop tracking one or more existing manifest.json urls"})
+
+      on :loaded, :start_timer
+
+      def start_timer(payload)
+        every(MINUTE) do
+          Lita::Timing::RateLimit.new("lita-asset-track", redis).once_every(HOUR) do
+            redis.sscan_each(MANIFEST_SET_KEY).map { |manifest_url|
+              URI(manifest_url)
+            }.each { |manifest_uri|
+              manifest_json = JSON.parse Net::HTTP.get(manifest_uri)
+
+              manifest_json.select { |k, v|
+                k.match /\.(#{EXTENSIONS.join("|")})$/
+              }.map { |asset_name, asset_path|
+                asset_uri = determine_asset_uri(manifest_uri, asset_path)
+
+                Net::HTTP.start(asset_uri.host, asset_uri.port) { |http|
+                  bytes = http.head(asset_uri.path)["content-length"]
+                  robot.trigger(:asset_track_size, host: manifest_uri.host, asset: asset_name, size: bytes)
+                }
+              }
+            }
+          end
+        end
+      end
+
+      def list(response)
+        manifests = redis.sscan_each(MANIFEST_SET_KEY).to_a
+
+        if manifests.any?
+          response.reply("Tracking:\n\t#{manifests.join("\n\t")}\n")
+        else
+          response.reply("Not tracking any manifests.")
+        end
+      end
+
+      def add(response)
+        urls = URI.extract(response.args.join(" "))
+
+        valid, invalid = urls.partition do |url|
+          url.end_with? "manifest.json"
+        end
+
+        response.reply("URLs must point to manifest.json, skipping: \n#{invalid.join("\n")}") if invalid.any?
+
+        redis.sadd(MANIFEST_SET_KEY, valid)
+        total = redis.scard(MANIFEST_SET_KEY)
+
+        response.reply("Added #{valid.count} URLs. Now tracking #{total} in total.")
+      rescue
+        response.reply("give proper url pls")
+      end
+
+      def remove(response)
+        redis.srem(MANIFEST_SET_KEY, URI.extract(response.args.join(" ")))
+        response.reply("Done")
+      rescue
+        response.reply("give proper url pls")
+      end
+
+      private
+
+      def determine_asset_uri(manifest_uri, asset_path)
+        if asset_path =~ URI::regexp
+          URI(asset_path)
+        else
+          URI::HTTP.build({
+            host: manifest_uri.host,
+            scheme: manifest_uri.scheme,
+            path: asset_path
+          })
+        end
+      end
+
+      Lita.register_handler(self)
     end
   end
 end
--- spec/lita-asset-track_spec.rb
+++ spec/lita-asset-track_spec.rb
@@ -1,5 +1,105 @@
+require 'redis'
 require 'lita-asset-track'
 
-RSpec.describe Lita::Handlers::AssetTrack do
-  it "is a thing"
+RSpec.describe Lita::Handlers::AssetTrack, lita_handler: true do
+  let(:handler) { Lita::Handlers::AssetTrack.new(robot) }
+  let(:redis_double) { instance_double(Redis) }
+  let(:response_double) { instance_double(Lita::Response) }
+
+  # stub out redis
+  before { allow(handler).to receive(:redis).and_return(redis_double) }
+
+  it "responds to 'asset track list'" do
+    expect(handler).to route("lita asset track list").to(:list)
+  end
+
+  it "responds to 'asset track add'" do
+    expect(handler).to route("lita asset track add xxx").to(:add)
+  end
+
+  it "responds to 'asset track remove'" do
+    expect(handler).to route("lita asset track remove xxx").to(:remove)
+  end
+
+  describe "#start_timer" do
+    it "sets a timer to run every hour" do
+      expect(handler).to receive(:every).with(60)
+      handler.start_timer(nil)
+    end
+  end
+
+  describe "#list" do
+    context "no manifests" do
+      it "tells the user" do
+        expect(redis_double).to receive(:sscan_each).and_return []
+        expect(response_double).to receive(:reply).with "Not tracking any manifests."
+
+        handler.list(response_double)
+      end
+    end
+
+    context "more than zero manifests in set" do
+      it "lists them to the user" do
+        expect(redis_double).to receive(:sscan_each).and_return ["https://example.com/manifest.json", "https://bing.com/manifest.json"]
+        expect(response_double).to receive(:reply).with <<~TEXT
+          Tracking:
+          \thttps://example.com/manifest.json
+          \thttps://bing.com/manifest.json
+        TEXT
+
+        handler.list(response_double)
+      end
+    end
+  end
+
+  describe "#add" do
+    context "valid url" do
+      it "adds it to the set" do
+        expect(response_double).to receive(:args).and_return ["https://theconversation.com/assets/manifest.json"]
+        expect(response_double).to receive(:reply).with("Added 1 URLs. Now tracking 5 in total.")
+        expect(redis_double).to receive(:scard).and_return 5
+        expect(redis_double).to receive(:sadd).with("lita-asset-track-manifest", ["https://theconversation.com/assets/manifest.json"])
+
+        handler.add(response_double)
+      end
+    end
+
+    context "invalid url" do
+      it "informs the user of their grievous error" do
+        expect(response_double).to receive(:args).and_return ["http://example.com/image.jpg"]
+        expect(response_double).to receive(:reply).with("URLs must point to manifest.json, skipping: \nhttp://example.com/image.jpg")
+        expect(response_double).to receive(:reply).with("Added 0 URLs. Now tracking 5 in total.")
+        expect(redis_double).to receive(:scard).and_return 5
+        expect(redis_double).to receive(:sadd).with("lita-asset-track-manifest", [])
+
+        handler.add(response_double)
+      end
+    end
+  end
+
+  describe "#remove" do
+    context "valid urls" do
+      let(:urls) { ["http://example.com/manifest.json", "http://fun.com/manifest.json"] }
+
+      it "is removed from the set" do
+        expect(response_double).to receive(:args).and_return urls
+        expect(response_double).to receive(:reply).with("Done")
+        expect(redis_double).to receive(:srem).with("lita-asset-track-manifest", urls)
+
+        handler.remove(response_double)
+      end
+    end
+
+    context "invalid urls" do
+      let(:urls) { ["rubbish", "🍾"] }
+
+      it "does nothing" do
+        expect(response_double).to receive(:args).and_return urls
+        expect(response_double).to receive(:reply).with("Done")
+        expect(redis_double).to receive(:srem).with("lita-asset-track-manifest", [])
+
+        handler.remove(response_double)
+      end
+    end
+  end
 end
